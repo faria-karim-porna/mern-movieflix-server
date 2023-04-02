b00700035fe39d8b81430b6c904fa672cb84f31b
faria-karim-porna <faria.porna.08@gmail.com>
2023-04-02 07:27:43 +0000

    Slightly Updated Response To Add Bookings Api

--- dist/index.js
+++ dist/index.js
@@ -35,7 +35,7 @@ client
     app.post("/addBookings", (req, res) => {
         const newBooking = req.body;
         bookingsCollection.insertOne(newBooking).then((result) => {
-            res.status(200).send("Successfully Add Bookings");
+            res.send(result.insertedCount);
         });
     });
     app.patch("/updateStatus", (req, res) => {

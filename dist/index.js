313c2ec3a4ed6c6ce78486b293b13217af69f7f8
faria-karim-porna <faria.porna.08@gmail.com>
2023-04-06 09:00:43 +0000

    Added Connection File

--- dist/index.js
+++ dist/index.js
@@ -7,12 +7,11 @@ const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const cors_1 = __importDefault(require("cors"));
 const dotenv_1 = __importDefault(require("dotenv"));
-const mongoose_1 = __importDefault(require("mongoose"));
 const routes_1 = __importDefault(require("./routes"));
+const connection_1 = __importDefault(require("./connection"));
 // import MongoClient from "mongodb".MongoClient;
 dotenv_1.default.config();
 // const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qs1yz.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-const uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-shard-00-00.qs1yz.mongodb.net:27017,cluster0-shard-00-01.qs1yz.mongodb.net:27017,cluster0-shard-00-02.qs1yz.mongodb.net:27017/${process.env.DB_NAME}?ssl=true&replicaSet=atlas-6yqhwu-shard-0&authSource=admin&retryWrites=true&w=majority`;
 const app = (0, express_1.default)();
 app.use(body_parser_1.default.json());
 app.use((0, cors_1.default)());
@@ -21,12 +20,5 @@ const port = 5000;
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
-const options = { useNewUrlParser: true, useUnifiedTopology: true };
-// mongoose.set("useFindAndModify", false);
-mongoose_1.default
-    .connect(uri)
-    .then(() => console.log("connected"))
-    .catch((error) => {
-    throw error;
-});
+(0, connection_1.default)();
 app.listen(process.env.PORT || port);
--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,17 @@ import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import router from "./routes";
+import connection from "./connection";
 // import MongoClient from "mongodb".MongoClient;
 
 dotenv.config();
 
 // const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qs1yz.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-const uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-shard-00-00.qs1yz.mongodb.net:27017,cluster0-shard-00-01.qs1yz.mongodb.net:27017,cluster0-shard-00-02.qs1yz.mongodb.net:27017/${process.env.DB_NAME}?ssl=true&replicaSet=atlas-6yqhwu-shard-0&authSource=admin&retryWrites=true&w=majority`;
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
-app.use(router)
+app.use(router);
 
 const port = 5000;
 
@@ -22,13 +22,6 @@ app.get("/", (req: any, res: any) => {
   res.send("Hello World!");
 });
 
-const options = { useNewUrlParser: true, useUnifiedTopology: true };
-// mongoose.set("useFindAndModify", false);
-mongoose
-  .connect(uri)
-  .then(() => console.log("connected"))
-  .catch((error: any) => {
-    throw error;
-  });
+connection();
 
 app.listen(process.env.PORT || port);
